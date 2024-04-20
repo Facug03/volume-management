24ecd8f8d9ed9893dc569db59b360eacad7a4221
Facug03 <facundogonzales1550@gmail.com>
2024-04-19 22:59:40 -0300

    listen to changes in the dom with MutationObserveer

--- src/volume.js
+++ src/volume.js
@@ -1,37 +1,51 @@
 console.log('xd')
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('test')
-
-  initObserver()
-})
+initObserver()
 
 function initObserver() {
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
-      if (mutation.type === 'childList') {
-        mutation.addedNodes.forEach((node) => {
-          if (node.nodeType === Node.ELEMENT_NODE) {
-            if (node.tagName === 'AUDIO' || node.tagName === 'VIDEO') {
-              console.log(node.tagName)
-              controlVolume()
-            }
+      // if (mutation.type === 'childList') {
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          if (node.tagName === 'AUDIO' || node.tagName === 'VIDEO') {
+            console.log(node.tagName)
+            controlVolume(node)
           }
-        })
-      }
+        }
+      })
+      // }
     })
   })
 
-  observer.observe(document.documentElement, { childList: true, subtree: true })
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  })
 }
 
-function controlVolume() {
+function controlVolume(mediaElement) {
+  const audioContext = new AudioContext()
+
+  const mediaSource = audioContext.createMediaElementSource(mediaElement)
+
+  const gainNode = audioContext.createGain()
+
+  mediaSource.connect(gainNode)
+
+  gainNode.connect(audioContext.destination)
+
+  gainNode.gain.value = 0
+}
+
+function searchAllVideosAndAudios() {
+  console.log({ document })
   const $soundElements = [...document.querySelectorAll('video, audio')]
 
   console.log($soundElements)
   if ($soundElements.length > 0) {
     $soundElements.forEach((element) => {
-      element.volume = 0
+      controlVolume(element)
     })
   }
 }
