a835820c71f3852ebe5afa2083df0f4acbb79a1c
Facug03 <facundogonzales1550@gmail.com>
2024-04-24 16:21:19 -0300

    add prettier and eslint configuration

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+export default {
+	printWidth: 100,
+	semi: false,
+	singleQuote: true,
+	quoteProps: 'consistent',
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+	endOfLine: 'lf',
+	arrowParens: 'always',
+}
--- src/popup.js
+++ src/popup.js
@@ -1,67 +1,67 @@
 function controlVolume() {
-  const $volume = document.getElementById('volume-slider')
-  const $img = document.getElementById('web-icon')
-  const $p = document.querySelector('p')
+	const $volume = document.getElementById('volume-slider')
+	const $img = document.getElementById('web-icon')
+	const $p = document.querySelector('p')
 
-  if (!$volume || !$img || !$p) return
+	if (!$volume || !$img || !$p) return
 
-  browser.tabs
-    .query({
-      currentWindow: true,
-      active: true,
-    })
-    .then((tabs) => {
-      for (const tab of tabs) {
-        const hostname = new URL(tab.url).hostname
+	browser.tabs
+		.query({
+			currentWindow: true,
+			active: true,
+		})
+		.then((tabs) => {
+			for (const tab of tabs) {
+				const hostname = new URL(tab.url).hostname
 
-        browser.storage.local.get(hostname).then((res) => {
-          const storage = res[hostname]
-          const storageVolume = storage?.volume
-          const storageFavicon = storage?.favicon
+				browser.storage.local.get(hostname).then((res) => {
+					const storage = res[hostname]
+					const storageVolume = storage?.volume
+					const storageFavicon = storage?.favicon
 
-          if (storageFavicon?.length > 0) {
-            $img.src = storageFavicon
-          } else {
-            $img.style = 'display: none;'
-          }
+					if (storageFavicon?.length > 0) {
+						$img.src = storageFavicon
+					} else {
+						$img.style = 'display: none;'
+					}
 
-          if (storageVolume) {
-            const stringVolume = (storageVolume * 100).toString()
-            $volume.value = stringVolume
-            $p.innerText = stringVolume
-          } else {
-            $p.innerText = '100'
-          }
-        })
-      }
-    })
-    .catch((err) => console.error(err))
+					if (storageVolume) {
+						const stringVolume = (storageVolume * 100).toString()
+						$volume.value = stringVolume
+						$p.innerText = stringVolume
+					} else {
+						$p.innerText = '100'
+					}
+				})
+			}
+		})
+		.catch((err) => console.error(err))
 
-  $volume.addEventListener('change', (e) => {
-    const currentVolume = e.target.value / 100
+	$volume.addEventListener('change', (e) => {
+		const currentVolume = e.target.value / 100
 
-    browser.tabs
-      .query({
-        currentWindow: true,
-        active: true,
-      })
-      .then((tabs) => {
-        console.log({ tabs })
-        for (const tab of tabs) {
-          browser.tabs
-            .sendMessage(tab.id, {
-              action: 'changeVolume',
-              data: currentVolume,
-            })
-            .catch((err) => console.error(err))
-        }
-      })
-      .catch((err) => console.error(err))
-  })
+		browser.tabs
+			.query({
+				currentWindow: true,
+				active: true,
+			})
+			.then((tabs) => {
+				console.log({ tabs })
+				for (const tab of tabs) {
+					browser.tabs
+						.sendMessage(tab.id, {
+							action: 'changeVolume',
+							data: currentVolume,
+						})
+						.catch((err) => console.error(err))
+				}
+			})
+			.catch((err) => console.error(err))
+	})
 
-  $volume.addEventListener('input', (event) => {
-    $p.innerText = event.target.value
-  })
+	$volume.addEventListener('input', (event) => {
+		$p.innerText = event.target.value
+	})
 }
 
 controlVolume()
