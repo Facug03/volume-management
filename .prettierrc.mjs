52687a2fdf24d93d0df1a3c8c5f103e830e8a3cb
Facug03 <facundogonzales1550@gmail.com>
2024-04-24 16:27:28 -0300

    change prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,12 +1,12 @@
 /** @type {import("prettier").Config} */
 export default {
-	printWidth: 100,
-	semi: false,
-	singleQuote: true,
-	quoteProps: 'consistent',
-	tabWidth: 2,
-	trailingComma: 'es5',
-	useTabs: true,
-	endOfLine: 'lf',
-	arrowParens: 'always',
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  endOfLine: 'lf',
+  arrowParens: 'always',
 }
--- src/volume.js
+++ src/volume.js
@@ -4,109 +4,109 @@ const gainNodesMap = new Map()
 let observer
 
 browser.runtime.onMessage.addListener((message) => {
-	if (message.action === 'changeVolume') {
-		let favicon = document.querySelector('link[rel="icon"]')?.href ?? ''
+  if (message.action === 'changeVolume') {
+    let favicon = document.querySelector('link[rel="icon"]')?.href ?? ''
 
-		if (favicon.length === 0) {
-			favicon = document.querySelector('link[rel~="icon"]')?.href ?? ''
-		}
+    if (favicon.length === 0) {
+      favicon = document.querySelector('link[rel~="icon"]')?.href ?? ''
+    }
 
-		browser.storage.local.set({ [hostname]: { volume: message.data, favicon } })
+    browser.storage.local.set({ [hostname]: { volume: message.data, favicon } })
 
-		getVideosAndAudios(document.documentElement)
-		initObserver()
-	}
+    getVideosAndAudios(document.documentElement)
+    initObserver()
+  }
 })
 
 browser.storage.local.get(hostname).then((res) => {
-	const storageVolume = res[hostname]?.volume
-	const storageFavicon = res[hostname]?.favicon?.length > 0 ? res[hostname].favicon : ''
+  const storageVolume = res[hostname]?.volume
+  const storageFavicon = res[hostname]?.favicon?.length > 0 ? res[hostname].favicon : ''
 
-	if (!storageVolume) return
+  if (!storageVolume) return
 
-	if (storageVolume === 1) return
+  if (storageVolume === 1) return
 
-	browser.storage.local.set({
-		[hostname]: { volume: storageVolume, favicon: storageFavicon },
-	})
+  browser.storage.local.set({
+    [hostname]: { volume: storageVolume, favicon: storageFavicon },
+  })
 
-	getVideosAndAudios(document.documentElement)
-	initObserver()
+  getVideosAndAudios(document.documentElement)
+  initObserver()
 })
 
 document.addEventListener('click', () => {
-	// checkear si location.href cambió, si es asi reiniciar gainNodesMap
+  // checkear si location.href cambió, si es asi reiniciar gainNodesMap
 
-	if (audioContext.state === 'suspended') {
-		audioContext.close()
-		gainNodesMap.clear()
-		audioContext = new AudioContext()
-		getVideosAndAudios(document.documentElement)
-	}
+  if (audioContext.state === 'suspended') {
+    audioContext.close()
+    gainNodesMap.clear()
+    audioContext = new AudioContext()
+    getVideosAndAudios(document.documentElement)
+  }
 })
 
 function initObserver() {
-	if (observer) return
+  if (observer) return
 
-	observer = new MutationObserver((mutations) => {
-		const uniqueParents = new Set()
-		const uniqueGrandParents = new Set()
+  observer = new MutationObserver((mutations) => {
+    const uniqueParents = new Set()
+    const uniqueGrandParents = new Set()
 
-		mutations.forEach((mutation) => {
-			if (mutation.type === 'childList') {
-				mutation.addedNodes.forEach((node) => {
-					if (node.nodeType !== Node.ELEMENT_NODE) return
+    mutations.forEach((mutation) => {
+      if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach((node) => {
+          if (node.nodeType !== Node.ELEMENT_NODE) return
 
-					uniqueParents.add(node.parentNode)
-				})
-			}
-		})
+          uniqueParents.add(node.parentNode)
+        })
+      }
+    })
 
-		if (uniqueParents.size === 0) return
+    if (uniqueParents.size === 0) return
 
-		uniqueParents.forEach((parent) => {
-			if (!parent || !parent.querySelectorAll) return
+    uniqueParents.forEach((parent) => {
+      if (!parent || !parent.querySelectorAll) return
 
-			uniqueGrandParents.add(parent.parentNode)
-		})
+      uniqueGrandParents.add(parent.parentNode)
+    })
 
-		uniqueGrandParents.forEach((parent) => {
-			if (!parent || !parent.querySelectorAll) return
+    uniqueGrandParents.forEach((parent) => {
+      if (!parent || !parent.querySelectorAll) return
 
-			getVideosAndAudios(parent)
-		})
-	})
+      getVideosAndAudios(parent)
+    })
+  })
 
-	observer.observe(document.documentElement, {
-		childList: true,
-		subtree: true,
-	})
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  })
 }
 
 function controlVolume(mediaElement) {
-	browser.storage.local.get(hostname).then((res) => {
-		const volume = res[hostname].volume
+  browser.storage.local.get(hostname).then((res) => {
+    const volume = res[hostname].volume
 
-		let gainNode = gainNodesMap.get(mediaElement)
+    let gainNode = gainNodesMap.get(mediaElement)
 
-		if (!gainNode) {
-			gainNode = audioContext.createGain()
-			const mediaSource = audioContext.createMediaElementSource(mediaElement)
-			mediaSource.connect(gainNode)
-			gainNode.connect(audioContext.destination)
-			gainNodesMap.set(mediaElement, gainNode)
-		}
+    if (!gainNode) {
+      gainNode = audioContext.createGain()
+      const mediaSource = audioContext.createMediaElementSource(mediaElement)
+      mediaSource.connect(gainNode)
+      gainNode.connect(audioContext.destination)
+      gainNodesMap.set(mediaElement, gainNode)
+    }
 
-		gainNode.gain.value = volume
-	})
+    gainNode.gain.value = volume
+  })
 }
 
 function getVideosAndAudios(element) {
-	const $soundElements = element.querySelectorAll('video, audio')
+  const $soundElements = element.querySelectorAll('video, audio')
 
-	if ($soundElements.length > 0) {
-		$soundElements.forEach((element) => {
-			controlVolume(element)
-		})
-	}
+  if ($soundElements.length > 0) {
+    $soundElements.forEach((element) => {
+      controlVolume(element)
+    })
+  }
 }
